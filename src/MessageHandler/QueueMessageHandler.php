97327b1a5471d979d6f58ed00af370e984355e26
Giuseppe <giuseppe@techtribe.nl>
2020-09-17 13:12:07 +0200

    using new provider

--- src/MessageHandler/QueueMessageHandler.php
+++ src/MessageHandler/QueueMessageHandler.php
@@ -2,14 +2,29 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\QueuePurchaseTokens;
 use App\Message\QueueMessage;
+use App\Service\JWTProvider;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class QueueMessageHandler implements MessageHandlerInterface
 {
+    private EntityManagerInterface $em;
+    private JWTProvider $JWTProvider;
+
+    public function __construct(EntityManagerInterface $em, JWTProvider $JWTProvider)
+    {
+        $this->em = $em;
+        $this->JWTProvider = $JWTProvider;
+    }
+
     public function __invoke(QueueMessage $message)
     {
-        var_dump($message->getToken());
-        die;
+        $queueToken = $message->getToken();
+        $queuePurchaseTokens = new QueuePurchaseTokens($queueToken, $this->JWTProvider->createPurchaseToken());
+        $this->em->persist($queuePurchaseTokens);
+        $this->em->flush();
     }
+
 }
